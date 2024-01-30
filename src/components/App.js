928212f50da77a599175ea6dc628803b49e95ca7
Julio Legrec <julio.legrec@codeworldwide.com>
2024-01-30 20:49:33 +0400

    cleanup

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import ApolloClient from 'apollo-boost';
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { createUploadLink } from 'apollo-upload-client';
-import { ApolloProvider } from 'react-apollo';
-import Home from './Home';
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { createUploadLink } from "apollo-upload-client";
+import { ApolloProvider } from "react-apollo";
+import Home from "./Home";
 // import Admin from './Admin';
-import Staff from './Staff';
-import Teacher from './Teacher';
-import Student from './Student';
-import NotFound from './NotFound';
+import Staff from "./Staff";
+import Teacher from "./Teacher";
+import Student from "./Student";
+import NotFound from "./NotFound";
 // import { ProtectedAdminRoute } from './ProtectedAdminRoute';
-import { ProtectedStaffRoute } from './ProtectedStaffRoute';
-import { ProtectedTeacherRoute } from './ProtectedTeacherRoute';
-import { ProtectedStudentRoute } from './ProtectedStudentRoute';
+import { ProtectedStaffRoute } from "./ProtectedStaffRoute";
+import { ProtectedTeacherRoute } from "./ProtectedTeacherRoute";
+import { ProtectedStudentRoute } from "./ProtectedStudentRoute";
 
 // Instantiate required constructor fields
 const cache = new InMemoryCache();
 
-const production = true;
+const production = false;
 
 const uploadLink = createUploadLink({
 	uri: production
-		? 'https://langdon-ims-server.herokuapp.com/graphql'
-		: 'http://localhost:5000/graphql',
+		? "https://langdon-ims-server.herokuapp.com/graphql"
+		: "http://localhost:5000/graphql",
 	headers: {
-		'keep-alive': 'true',
+		"keep-alive": "true",
 	},
 });
 
 const defaultOptions = {
 	watchQuery: {
-		fetchPolicy: 'no-cache',
-		errorPolicy: 'ignore',
+		fetchPolicy: "no-cache",
+		errorPolicy: "ignore",
 	},
 	query: {
-		fetchPolicy: 'no-cache',
-		errorPolicy: 'all',
+		fetchPolicy: "no-cache",
+		errorPolicy: "all",
 	},
 };
 
